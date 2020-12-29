9cb3808fe820947108c15a778dc34f3f054ecb54
Marcelo Felix <celofelix93@gmail.com>
2020-12-29 10:38:15 -0300

    Conectndo ao MongoDB com repository e service

---
+++ src/main/java/com/educandoweb/springemongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.educandoweb.springemongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.educandoweb.springemongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
