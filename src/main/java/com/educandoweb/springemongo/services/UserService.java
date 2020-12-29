b44a7a6e95ee1457d5c6c2a7dce6a777ff584d51
Marcelo Felix <celofelix93@gmail.com>
2020-12-29 11:44:43 -0300

    Obtendo um usuário por id e criado tratamento de excecao personalizada

--- src/main/java/com/educandoweb/springemongo/services/UserService.java
+++ src/main/java/com/educandoweb/springemongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.educandoweb.springemongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.springemongo.domain.User;
 import com.educandoweb.springemongo.repository.UserRepository;
+import com.educandoweb.springemongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return userRepository.findAll();		
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto com o ID não encontrado"));
 	}
 	
 }
