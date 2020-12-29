41c1c767b2ffe53a8de75ccabf6779cee06f0e1f
Marcelo Felix <celofelix93@gmail.com>
2020-12-29 12:08:13 -0300

    Insercao de usuario com POST e resposta no headers com a classe URI

--- src/main/java/com/educandoweb/springemongo/services/UserService.java
+++ src/main/java/com/educandoweb/springemongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.springemongo.domain.User;
+import com.educandoweb.springemongo.dto.UserDTO;
 import com.educandoweb.springemongo.repository.UserRepository;
 import com.educandoweb.springemongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto com o ID não encontrado"));
+	}
+	
+	public User insert(User user) {
+		return userRepository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 	
 }
